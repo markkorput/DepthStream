6ba9bad2f847ff7aa6ec97f80e468bca173962b5
mark <mark.korput@gmail.com>
2020-03-01 17:07:16 +0100

    example-unityreceiver improvements and option for example_player to send uncompressed data

--- example-unityreceiver/Assets/DepthStream/Scripts/Populator.cs
+++ example-unityreceiver/Assets/DepthStream/Scripts/Populator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using Computils;
 
 namespace depth {
@@ -8,19 +9,27 @@ namespace depth {
     {
         public ComputeBufferFacade vectorBufferFacade;
 
+        public UnityEvent OnNewFrameSize = new UnityEvent();
+
+
         Vector3[] vec3s = null;
         int width;
         int height;
 
         System.Action<byte[], Vector3[], int, int> populatorFunc = null;
 
+
         public void PopulateVector3(Frame f) {
+            bool newSize = false;
+
             if (vec3s == null || vec3s.Length != f.Size) {
                 if (f.Size == (1280*720*2)) {
+                    Debug.Log("Allocating new vector buffer for 16bit 1280x720 stream");
                     width = 1280;
                     height = 720;
                     populatorFunc = populateVector3_16bit;
                     vec3s = new Vector3[f.Size];
+                    newSize = true;
                 } else {
                     Debug.LogWarning("Unsupported frame size: "+f.Size);
                     return;
@@ -31,6 +40,7 @@ namespace depth {
 
             Populate(vec3s);
             // Debug.Log("Populated "+width+"x"+height+" depth pixels");
+            if (newSize) OnNewFrameSize.Invoke();
         }
 
         void populateVector3_16bit(byte[] data, Vector3[] vector3s, int width, int height) {
